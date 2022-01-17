28a87506c8e75649543d3e734a1b3619597dc0d9
Arvin <gnimoaix857@gmail.com>
2022-01-17 18:47:14 +0700

    Added home page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:smart_bin_app/app/shared/constants/app_colors.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: AppColors.green,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Hampas Smart Bin',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox.square(
+              child: Image.asset('assets/images/hampas_logo.png'),
+              dimension: 200,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Welcome',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    child: const Text('Check Bin Status'),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(AppColors.black),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
---
+++ lib/app/shared/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const green = Color.fromRGBO(120, 170, 111, 1.0);
+  static const black = Color.fromRGBO(72, 82, 94, 1.0);
+}
